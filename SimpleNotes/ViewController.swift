d07e930f7ecbaead7bfa10caba766af4172fdc60
João Palma <joaowd@outlook.com>
2019-12-07 23:03:41 +0000

    Initial Commit

---
+++ SimpleNotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleNotes
+//
+//  Created by João Palma on 07/12/2019.
+//  Copyright © 2019 João Palma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
